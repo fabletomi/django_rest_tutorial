1b5bd80dc8e123f0dee8f229611097eac0c3bb85
fabletomi <fabletomi@gmail.com>
2023-02-27 17:58:22 +0100

    changes

--- rest_tutorial/rest_tutorial/urls.py
+++ rest_tutorial/rest_tutorial/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import include, path
+from django.urls import include
 from rest_framework import routers
 from rest_api import views
 
